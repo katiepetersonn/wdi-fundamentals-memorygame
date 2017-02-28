00daa6f48f7e316d1a3c32f554187d2e312e9fd1
katiepetersonn <katie.peterson19@gmail.com>
2017-02-28 15:20:56 +1100

    added alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,13 @@
-console.log("JS file is connected to HTML! Woo!")
+//alert ("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert = "You found a match!"
+}
+else {
+	alert = "Sorry, try again."
+}
