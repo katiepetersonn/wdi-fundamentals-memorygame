055c1c3bd98d78429f1c289d5fd0269f573adaac
katiepetersonn <katie.peterson19@gmail.com>
2017-03-14 14:20:44 +1100

    unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,6 @@
 //alert ("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
@@ -10,4 +10,20 @@ if (cardTwo === cardFour) {
 }
 else {
 	alert = "Sorry, try again."
+}*/
+//get an element with the id of game-board and set it to an element.
+ var boardGame = document.getElementById ('game-board'); {
+
+var createCards = function (){
+//create a for loop that creates HTML elements for each card
+	for (var i = 0; i<cards.length; i++) {
+//each card should be a div.
+	var cardElement = document.createElement('div');
+//and have the class card
+	cardElement.className = 'card';
+//append each newly created card to the div.
+	boardGame.appendChild(cardElement);
 }
+
+}
+createCards();
