ee3c6c5b80550143c80f1225d111c363941ca895
katiepetersonn <katie.peterson19@gmail.com>
2017-03-21 20:32:48 +1100

    Changed images of cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,59 +1,53 @@
-// represents the cards of the game
+//declare a variable, cards, and set it equal to an array of four string.
 var cards = ['queen', 'queen', 'king', 'king'];
 
-// represents the cards in play
+//declare a variable, cardsInPlay, whose value is an empty string.
 var cardsInPlay = [];
 
-
-
-// find the board and set it to a variable
-// so that we can add cards to it
 var board = document.getElementById('game-board');
-// function that will create your board
-function createBoard() {
-  // loop through your cards array to create card elements for your board
-  for (var i=0; i<cards.length; i++) {
-    // create a div element which will be used as a card
-    var cardElement = document.createElement('div');
-    // add a class to the card element which will help link styling
-    cardElement.className = 'card';
-    // this will set the card's 'data-card' to be the element of the array
-    // data- attributes are meant to store data about an element that is not tied to a style.
-    // i.e. "king"
-    cardElement.setAttribute('data-card', cards[i]);
-    // when a card is clicked the function isTwoCards will be executed
-    cardElement.addEventListener('click', isTwoCards);
 
-    // append the card to the board
+//set up each card with the createBoard function.
+function createBoard() {
+
+//loop through the cards array.
+  for (var i=0; i<cards.length; i++) {
+//div element that will be used as a card.
+    var cardElement = document.createElement('div');
+//add a class to the card element.
+    cardElement.className = 'card';
+
+//attribute that equals the card value.
+    cardElement.setAttribute('data-card', cards[i]);
+//click event listener to each card.
+  cardElement.addEventListener('click', isTwoCards);
+
+//append the card to the board
     board.appendChild(cardElement);
     board.appendChild(cardElement);
   }
 
 }
-//checks to see if there are cards in play
+
+//check to see if there are cards in play. 
+
 function isTwoCards() {
-  // add card to array of cards being viewed
-  // 'this' is not covered in the pre-work but
-  // for now, just know it gives you access to the cardElement you click on
   cardsInPlay.push(this.getAttribute('data-card'));
-  // show the card's image
+//show the card's image
   console.log(this.getAttribute('data-card'));
   if (this.getAttribute('data-card') === 'king') {
-    this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
+    this.innerHTML = "<img src='css/king.png'>"; // king
   } else {
-    this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
+    this.innerHTML = "<img src='css/queen.png'>"; //queen
   }
-  // if you have two cards in play check for a match
+//if you have two cards in play check for a match
   if (cardsInPlay.length === 2) {
-    // pass the cardsInPlay as an argument to isMatch function
     isMatch(cardsInPlay);
-    // clear cards in play array for next try
     cardsInPlay = [];
   }
 }
 
+//test if two cards in play are a match.
 function isMatch(cards) {
-  // alert winning message
   if (cards[0] === cards[1]) {
     alert("You found a match!");
   } else {
